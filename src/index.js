8049ab58f8248396634f02deadd1de44b1c042af
Ying Ying <yingyingfengg@gmail.com>
2022-03-07 22:42:59 -0500

    adding react and axios call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-ReactDOM.render(<hr />, document.querySelector('#root'))
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            pasta: []
+        }
+    }
+    async componentDidMount() {
+        const pasta = (await axios.get('/api/pasta')).data
+        this.setState({ pasta })
+    }
+    render() {
+        const pasta = this.state.pasta;
+        return (
+            <div>
+                <h1>Sunsweet's Pasta Recommendations</h1>
+                <div>
+                    {
+                        pasta.map( pasti => {
+                            return (
+                                <p>
+                                    { pasti.name }
+                                </p>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
