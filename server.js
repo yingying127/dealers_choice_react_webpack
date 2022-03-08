48464d62821a9d74351cd16b8a93da8cfa9c57e1
Ying Ying <yingyingfengg@gmail.com>
2022-03-07 22:11:45 -0500

    added faker wit get and post routes phase 2

--- server.js
+++ server.js
@@ -10,13 +10,40 @@ const Pasta = sequelize.define('pasta', {
 
 Pasta.createName = (name) => Pasta.create({ name })
 
+const express = require('express');
+const app = express();
+
+const faker = require('faker');
+
+app.post('/api/pasta', async(req, res, next) => {
+    try {
+        const city = faker.address.city()
+        res.send(await city)
+    }
+    catch(ex) {
+        next(ex)
+    }
+})
+
+app.get('/api/pasta', async(req, res, next) => {
+    try {
+        res.send(await Pasta.findAll())
+    }
+    catch(ex) {
+        next(ex)
+    }
+})
+
 const syncAndSeed = async() => {
     try {
         await sequelize.sync({ force: true });
 
         const [ziti, fusilli, spaghetti, ravioli, tortellini] = await Promise.all(
             ['Baked Ziti', 'Fusili alla Caprese', 'Spaghetti Cacio e Pepe', 'Ravioli ai Piselli', 'Tortellini in Brodo'].map(Pasta.createName)
-        )        
+        )
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`listening on port ${port}`))
     }
     catch(ex) {
         console.log(ex)
